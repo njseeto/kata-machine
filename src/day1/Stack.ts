6e74e5f3287197572381eebd7f861f40c1895b72
Natalie <njseeto@users.noreply.github.com>
2023-02-17 16:51:23 +0000

    Stacks
    Stacks use LIFO rule.
    That means push and pop are O(1).
    For a queue we push to the end/tail and pop of the end and tail.

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,43 @@
+type Node<T> = {
+  value: T,
+  previous?: Node<T>
+}
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const node = {value: item} as Node<T>
+    if (!this.head) {
+      this.head = node
+      return;
+    }
+    node.previous = this.head;
+    this.head = node;
+    this.length ++;
+  }
+
+  pop(): T | undefined {
+    if (this.length === 0) {
+      const head = this.head;
+      this.head = undefined;
+      return head?.value;
     }
 
-    push(item: T): void {
+    const head = this.head as Node<T>;
+    this.head = head.previous;
+    this.length--;
 
-}
-    pop(): T | undefined {
+    head.previous = undefined;
+    return head.value;
+  }
 
-}
-    peek(): T | undefined {
-
-}
+  peek(): T | undefined {
+    return this.head?.value
+  }
 }
