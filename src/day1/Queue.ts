1df35a826afb8566c1ae8b5a83d719848fd228bf
Natalie <njseeto@users.noreply.github.com>
2023-02-17 15:17:57 +0000

    Queue
    Queue class, constructor, enqueue, deque, peek.
    These methods are O(1).
    Queues operate on FIFO.

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node = {value: item} as Node<T>;
+    if (!this.tail) {
+      // create a node
+      this.head = this.tail = node;
+      return;
+    }
+    this.length++;
+
+    this.tail.next = node;
+    this.tail = node;
+  }
+  deque(): T | undefined {
+    if (!this.head) { 
+      return undefined;
     }
 
-    enqueue(item: T): void {
+    const head = this.head;
+    this.head = this.head.next;
+    head.next = undefined;
 
-}
-    deque(): T | undefined {
-
-}
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+    this.length--;
+    return head.value;
+  }
     peek(): T | undefined {
-
-}
+      return this.head?.value // ? is a safe navigation operator
+  }
 }
