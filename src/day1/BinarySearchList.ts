2c4a033508c661d076bafee5969bc36913e4b3af
Natalie <njseeto@users.noreply.github.com>
2023-02-17 11:29:00 +0000

    Binary search
    O(log N)
    
    Uses low and high variables, a midpoint and a value (the value
    of th emidpoint).

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  // find the midpoint, assign high and low variables
+  let low = 0;
+  let high = haystack.length;
 
+  do {
+    const midpoint = Math.floor(low + (high - low)/2);
+    const value = haystack[midpoint];
+
+    if (value === needle){
+      return true;
+    } else if (value > needle){
+      high = midpoint;
+    } else {
+      low = midpoint + 1;
+    }
+
+  } while (low < high)
+
+  return false
 }
