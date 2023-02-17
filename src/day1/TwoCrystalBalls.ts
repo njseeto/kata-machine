ec68989a403a1a2f96ebb6c26b1620463edbdbc9
Natalie <njseeto@users.noreply.github.com>
2023-02-17 11:53:07 +0000

    Crystal Ball Problem
    O(sqrt N)

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jump_amount = Math.floor(Math.sqrt(breaks.length));
 
+  let i = jump_amount
+
+  // jump forward by square root on N until we find the break
+  for(; i < breaks.length; i += jump_amount){
+    if (breaks[i]){
+      break;
+    }
+  }
+
+  // jump back square root on N
+  i -= jump_amount;
+
+  // walk forward square root on N
+  for(let j = 0; j <= jump_amount && i < breaks.length; ++j, ++i){
+    if(breaks[i]){
+      return i;
+    }
+  }
+
+  return -1
 }
