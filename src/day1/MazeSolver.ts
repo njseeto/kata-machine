c79d9812c6c69a3fc3d13f8cf0ff6c5724455bb9
Natalie <njseeto@users.noreply.github.com>
2023-02-20 18:14:22 +0000

    Recursion
    Add base cases to map solver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,23 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]): boolean {
+  // describe the base cases
+  // off the map
+  if (curr.x < 0 || curr.x >= maze[0].length ||
+      curr.y < 0 || curr.y >= maze[0].length) {
+      return false;
+    }
+  // on a wall
+  if (maze[curr.y][curr.x] === wall) {
+    return false;
+  }
+  // at the end
+  if (curr.x === end.x && curr.y == end.y) {
+    return true;
+  }
+  // seen the point before
+  if(seen[curr.y][curr.x]){
+    return false;
+  }
+}
 
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 }
