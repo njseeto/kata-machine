56ae5b318953c6030c6f1a42749cc3523f31f9c6
Natalie <njseeto@users.noreply.github.com>
2023-02-20 18:33:28 +0000

    Maze Solver - recurse steps
    Remember we have a pre, recurse and post steps.
    First we do a setup/pre step, then we recurse, then we do a post step.
    
    Pre step here:
    We take the current point and ensure it is true in the seen 2D array.
    Then push these co-ords into the path array.
    
    Recurse:
    Take the current point and check all four directions (up, right, down, left)
    Return true if there is a path.
    
    Post:
    Pop off the last point from the path array.

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,4 +1,10 @@
-function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]): boolean {
+const dir = [
+  [-1, 0], // left
+  [1, 0], // right
+  [0, 1], // up
+  [0, -1] //down
+]
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
   // describe the base cases
   // off the map
   if (curr.x < 0 || curr.x >= maze[0].length ||
@@ -11,13 +17,46 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
   }
   // at the end
   if (curr.x === end.x && curr.y == end.y) {
+    path.push(end); // need to push in the last/end point 
     return true;
   }
   // seen the point before
   if(seen[curr.y][curr.x]){
     return false;
   }
+
+  // Recurse step. Pre, recurse, post steps
+  //pre - push current point into path
+  seen[curr.y][curr.x] = true; 
+  path.push(curr);
+  
+  //recurse - check possible directions
+  for(let i = 0; i < dir.length; i++) {
+    const [x, y] = dir[i]
+    if (walk(maze, wall, {
+        x: curr.x + x,
+        y: curr.y + y
+        }, end, seen, path
+        )
+      ) {
+        return true; // this adds the true to the seen path
+      }
+  }
+
+  //post - pop last point off
+  path.pop(); // this pops the current point off, and then uses the last seen point
+  return false; // if we recurse and we did not find end in the final position it would be false
 }
 
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+  const seen: boolean[][] = [];
+  const path: Point[] = [];
+
+  for (let i = 0; i < maze.length; i++) {
+    seen.push(new Array(maze[0].length).fill(false));
+  }
+
+  walk(maze, wall, start, end, seen, path)
+
+  return path;
 }
