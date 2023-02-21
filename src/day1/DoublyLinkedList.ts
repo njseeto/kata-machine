c0474d184e8e63cce747e24a5bc47ab6544f37e7
Natalie Seeto <njseeto@users.noreply.github.com>
2023-02-21 17:05:11 +0000

    Add debug

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -14,6 +14,16 @@ export default class DoublyLinkedList<T> {
     this.tail = undefined;
   }
 
+  private debug() {
+    let curr = this.head;
+    let out = "";
+    for (let i = 0; curr && i < this.length; i++) {
+      out += `${i} => ${curr.value} `;
+      curr = curr.next;
+    }
+    console.log(out);
+  }
+
   prepend(item: T): void {
     const node = {value: item} as Node<T>;
     this.length++;
@@ -58,12 +68,14 @@ export default class DoublyLinkedList<T> {
 
     if (!this.tail){
       this.head = this.tail = node;
+      this.debug();
       return;
     }
 
     node.prev = this.tail;
     this.tail.next = node;
     this.tail = node;
+    this.debug();
   }
 
   remove(item: T): T | undefined {
