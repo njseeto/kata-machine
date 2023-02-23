438bac793413aaef8c4ade89fcac77df41664b41
Natalie Seeto <njseeto@users.noreply.github.com>
2023-02-23 17:27:14 +0000

    Trees: Binary tree Post-Order traversal
    Post-order traversal traverses both subtrees of a node, then
    visits the node.
    
    The head of the tree will be the last node visited.

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,20 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+// Post-order we go left then right, then we visit the node
 
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  // base case is when a node does not exist
+  if (!curr) {
+    return path;
+  }
+
+  // recurse
+  walk(curr.left, path);
+  walk(curr.right, path);
+
+  // post
+  path.push(curr.value);
+  return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
