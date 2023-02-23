7448879a3bce0dd90678e7dd7288ac057e9c8776
Natalie Seeto <njseeto@users.noreply.github.com>
2023-02-23 17:25:14 +0000

    Trees: Binary Trees In Order Traversal
    In-order traversals traverses one subtree of a node, visits the
    node, and then traverses the other subtree.
    
    Here the head of the tree ends up in the middle position.

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,20 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+// In-order is when we go all the way left then push in the node then go right
 
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  // base case is when a node does not exist
+  if (!curr) {
+    return path;
+  }
+
+  // recurse
+  walk(curr.left, path);
+  path.push(curr.value);
+  walk(curr.right, path);
+
+  // post
+  return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
