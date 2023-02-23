ae33ac56ab84745d27e08563f6eea25548773190
Natalie <njseeto@users.noreply.github.com>
2023-02-23 18:34:19 +0000

    Breadth First Search
    - This should use a Queue
    - We do not use recursion here, instead we iterate

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,26 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+// This is the opposite to DFS and so should use a Queue
+// We don't need recursion, instead we take an iterative approach
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q: (BinaryNode<number> | null)[]  = [head];
+
+  while (q.length) {
+
+    // the types tell us what the type of the shifted value could be
+    const next = q.shift() as BinaryNode<number> | undefined | null;
+
+    if (!next) {
+      continue;
+    }
+
+    // search
+    if (next.value === needle) {
+      return true;
+    }
+
+    q.push(next.left);
+    q.push(next.right);
+  }
+
+  return false;
 }
