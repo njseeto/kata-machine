7b638fb88c8a5f94fcd8239bce10c806c75803bd
Natalie <njseeto@users.noreply.github.com>
2023-02-23 18:53:00 +0000

    Compare binary trees (data and structure)
    - Depth First Search preserves the shape/structure of the traversal
    
    So in this problem we would use a DFS.

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,22 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+// compare two binary trees to check if they are equal in value and structure
 
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+  // base cases
+  // if both are null - structurally they are the same
+  if (a === null && b === null) {
+    return true;
+  }
+
+  // if either is null - this means the trees have a different structure
+  if (a === null || b === null) {
+    return false;
+  }
+
+  // if the node values are not the same
+  if (a.value !== b.value) {
+    return false;
+  }
+
+  // recurse over the whole tree and the terminating nodes should be equal
+  return compare(a.left, b.left) && compare(a.right, b.right);
 }
