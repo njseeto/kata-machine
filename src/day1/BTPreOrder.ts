ac2269e599007d18e770c027b082a094a2c77e14
Natalie Seeto <njseeto@users.noreply.github.com>
2023-02-23 17:23:51 +0000

    Trees: Binary Tree Pre-Order Traversal
    A pre-order traversal visits a node, and the traverses both of its
    subtrees.
    
    Here we end up with the head of the tree in the zeroth position.

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,24 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+// Pre-order is when we push the node then move left and repeat this
+// until we reach the base case then move right
 
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  // base case is when a node does not exist
+  if (!curr) {
+    return path;
+  }
+
+  // recurse
+  // pre
+  path.push(curr.value);
+
+  // recurse
+  walk(curr.left, path);
+  walk(curr.right, path);
+
+  // post
+  return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
