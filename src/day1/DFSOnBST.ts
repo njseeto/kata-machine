71ee50701295e737dc6b7b1e37af60c7bbd35b3a
Natalie Seeto <njseeto@users.noreply.github.com>
2023-02-28 14:16:55 +0000

    DFS on Binary Search Tree
    Here we have two base cases, when the value is not present in the
    tree, ie: null, or when we find the needle.
    
    If neither of these are true then we continue to recurse.
    
    The runtime of this algorithm varies according to the height of
    the tree and how balanced the tree is.
    Worst case: O(n)
    Best case: O(log n)

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,22 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(node: BinaryNode<number> | null, needle: number): boolean {
+  // base case 1 - null case
+  if (node === null) {
+    return false;
+  }
 
+  // base case 2 - when the needle has been found
+  if (node.value === needle) {
+    return true;
+  }
+
+  // otherwise we recurse
+  if (node.value < needle){
+    return search(node.right, needle);
+  } else {
+    return search(node.left, needle);
+  }
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
 }
